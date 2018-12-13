84104a1254bf29261d78a7335ab5b573dca413ba
Yash <yasodha@rjtcompuquest.com>
2018-12-13 14:57:00 -0600

    Installed Bootstrap

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -9,9 +9,9 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
   /*spec*/
-/*  it('should create the app', async(() => {
+  it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));*/
+  }));
   });
